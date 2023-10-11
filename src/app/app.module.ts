1e0475564869f4218591ccbeed9a7b9735e89c45
= <=>
2023-10-11 22:14:50 +0100

    card component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatNativeDateModule} from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserAnimationsModule,
     MatNativeDateModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
